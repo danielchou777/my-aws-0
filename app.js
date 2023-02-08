26e7476f2d14cf22a960972bee43a8fd75208889
danielchou777 <danielchou777@gmail.com>
2023-02-08 23:35:02 +0800

    Fix: app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(8080, () => {
-  console.log('Server is listening on port 80...');
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is listening on port ${port}...`);
 });
